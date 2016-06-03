f8ae6d32a11e97109cf9ad1cb534fd83d60fff98
Jonathan Flatt <jonathanm.flatt@gmail.com>
2016-06-03 19:51:48 -0400

    forcing https connections for the WebAPI

---
+++ tipless-cash-jar/web/Core/EnforceHttpsHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace tiplessCashJar.web.Core
+{
+    public class EnforceHttpsHandler : DelegatingHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            object httpContextBaseObject;
+            if (request.Properties.TryGetValue("MS_HttpContext", out httpContextBaseObject))
+            {
+                var httpContextBase = httpContextBaseObject as HttpContextBase;
+
+                if (httpContextBase != null && httpContextBase.Request.IsLocal)
+                {
+                    return base.SendAsync(request, cancellationToken);
+                }
+            }
+            if (request.RequestUri.Scheme != Uri.UriSchemeHttps)
+            {
+                return Task<HttpResponseMessage>.Factory.StartNew(
+                    () =>
+                    {
+                        var response = new HttpResponseMessage(HttpStatusCode.Forbidden)
+                        {
+                            Content = new StringContent("HTTPS Required")
+                        };
+                        return response;
+                    });
+            }
+
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
---
+++ tipless-cash-jar/web/Core/RequireSecureConnectionFilter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Web.Mvc;
+
+
+
+namespace tiplessCashJar.web.Core
+{
+    public class RequireSecureConnectionFilter : RequireHttpsAttribute
+    {
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            if (filterContext == null)
+            {
+                throw new ArgumentNullException("filterContext");
+            }
+            if (filterContext.HttpContext.Request.IsLocal)
+            {
+                return;
+            }
+
+            base.OnAuthorization(filterContext);
+        }
+    }
+}
