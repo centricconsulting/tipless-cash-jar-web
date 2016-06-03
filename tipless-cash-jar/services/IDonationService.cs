376091dcd7dd2a8bfd741a439167e1b3e70399af
Jonathan Flatt <jonathanm.flatt@gmail.com>
2016-06-03 17:55:12 -0400

    added get donation to route

--- tipless-cash-jar/services/IDonationService.cs
+++ tipless-cash-jar/services/IDonationService.cs
@@ -7,24 +7,35 @@ using tiplessCashJar.services.models;
 
 namespace tiplessCashJar.services
 {
-  public interface IDonationService
-  {
-    Task<ExecutedDonationServiceModel> Execute(NewDonationServiceModel newDonation);
-  }
-
-  public class DonationService : IDonationService
-  {
-    public Task<ExecutedDonationServiceModel> Execute(NewDonationServiceModel newDonation)
+    public interface IDonationService
     {
-      throw new NotImplementedException();
+        Task<ExecutedDonationServiceModel> Execute(NewDonationServiceModel newDonation);
+        Task<ExecutedDonationServiceModel> GetById(Guid id);
     }
-  }
 
-  public class FakeDonationService : IDonationService
-  {
-    public async Task<ExecutedDonationServiceModel> Execute(NewDonationServiceModel newDonation)
+    public class DonationService : IDonationService
     {
-      return new ExecutedDonationServiceModel { Id = Guid.NewGuid() };
+        public Task<ExecutedDonationServiceModel> Execute(NewDonationServiceModel newDonation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ExecutedDonationServiceModel> GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
     }
-  }
+
+    public class FakeDonationService : IDonationService
+    {
+        public async Task<ExecutedDonationServiceModel> Execute(NewDonationServiceModel newDonation)
+        {
+            return new ExecutedDonationServiceModel { Id = Guid.NewGuid() };
+        }
+
+        public async Task<ExecutedDonationServiceModel> GetById(Guid id)
+        {
+            return new ExecutedDonationServiceModel { Id = id };
+        }
+    }
 }
--- tipless-cash-jar/web/Api/DonationController.cs
+++ tipless-cash-jar/web/Api/DonationController.cs
@@ -5,36 +5,52 @@ using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
+using System.Web.Http.Description;
 using tiplessCashJar.services;
 using tiplessCashJar.services.models;
 using tiplessCashJar.web.ApiModels;
 
 namespace tiplessCashJar.web.Api
 {
-  public class DonationController : ApiController
-  {
-    private IDonationService DonationService { get; set; }
-
-    public DonationController(IDonationService donationService) { DonationService = donationService; }
-
-    [HttpPost]
-    [Route("api/donate")]
-    public async Task<IHttpActionResult> Donate([FromBody] SendDonationApiModel donation)
+    public class DonationController : ApiController
     {
-      if (!ModelState.IsValid) return BadRequest("Donation model is invalid.");
+        private IDonationService DonationService { get; set; }
 
-      var newDonation = new NewDonationServiceModel { GiverId = Guid.NewGuid() };
-      newDonation = donation.ApplyTo(newDonation);
+        public DonationController(IDonationService donationService) { DonationService = donationService; }
 
-      try
-      {
-        var finalizedDonation = await DonationService.Execute(newDonation);
-        return CreatedAtRoute("DefaultApi", new { controller = "Donation", id = finalizedDonation.Id }, new CompletedDonationApiModel(finalizedDonation));
-      }
-      catch (Exception e)
-      {
-        return InternalServerError(e);
-      }
+        [HttpPost]
+        [Route("api/donate")]
+        public async Task<IHttpActionResult> Donate([FromBody] SendDonationApiModel donation)
+        {
+            if (!ModelState.IsValid) return BadRequest("Donation model is invalid.");
+
+            var newDonation = new NewDonationServiceModel { GiverId = Guid.NewGuid() };
+            newDonation = donation.ApplyTo(newDonation);
+
+            try
+            {
+                var finalizedDonation = await DonationService.Execute(newDonation);
+                return CreatedAtRoute("DefaultApi", new { controller = "Donation", id = finalizedDonation.Id }, new CompletedDonationApiModel(finalizedDonation));
+            }
+            catch (Exception e)
+            {
+                return InternalServerError(e);
+            }
+        }
+        [HttpGet]
+        [Route("api/donate")]
+        [ResponseType(typeof(CompletedDonationApiModel))]
+        public async Task<IHttpActionResult> Get(Guid id)
+        {
+
+            var model = await DonationService.GetById(id);
+            if (model == null)
+            {
+                return NotFound();
+            }
+            return Ok(new CompletedDonationApiModel(model));
+
+        }
+
     }
-  }
 }
