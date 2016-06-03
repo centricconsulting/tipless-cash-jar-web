14add7e862b738e422e258ad49f6506df4ab8388
Shawn Wallace <shawn@the-wallaces.net>
2016-06-03 15:42:49 -0400

    added base controllers

--- tipless-cash-jar/web/Core/ApiControllerBase.cs
+++ tipless-cash-jar/web/Core/ApiControllerBase.cs
@@ -3,15 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
-using System.Web.Mvc;
 
 namespace web.Core
 {
   public class AppApiControllerBase : ApiController
-  {
-  }
-
-  public class AppControllerBase : Controller
   {
   }
 }
---
+++ tipless-cash-jar/web/Core/AppControllerBase.cs
@@ -0,0 +1,8 @@
+using System.Web.Mvc;
+
+namespace web.Core
+{
+  public class AppControllerBase : Controller
+  {
+  }
+}
