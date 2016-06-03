51ca0efa508c3ad06441025457f226713c38d798
Shawn Wallace <shawn@the-wallaces.net>
2016-06-03 15:36:52 -0400

    initial project setup

---
+++ tipless-cash-jar/web/Core/ApiControllerBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace web.Core
+{
+  public class AppApiControllerBase : ApiController
+  {
+  }
+
+  public class AppControllerBase : Controller
+  {
+  }
+}
