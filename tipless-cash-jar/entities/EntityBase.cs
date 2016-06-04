d7d564480cc593a049ec39e7d89f1104c8ddb993
Shawn Wallace <shawn@the-wallaces.net>
2016-06-03 20:31:48 -0400

    Added entity conifg

---
+++ tipless-cash-jar/entities/EntityBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace tiplessCashJar.entities
+{
+    public class EntityBase<TKey> : IEntity<TKey>
+    {
+      public TKey Id { get; set; }
+      public DateTime WhenCreated { get; set; }
+      public DateTime? WhenModified { get; set; }
+    }
+}
---
+++ tipless-cash-jar/entities/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace tiplessCashJar.entities
+{
+  public interface IEntity<TKey>
+  {
+    TKey Id { get; set; }
+    DateTime WhenCreated { get; set; }
+    DateTime? WhenModified { get; set; }
+  }
+}
