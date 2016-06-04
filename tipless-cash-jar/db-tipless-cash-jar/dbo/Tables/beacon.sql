6a9847a88cc4f97aa2c8508daef7171e34151336
jsaalto <jeff.aalto@centricconsulting.com>
2016-06-04 16:34:45 -0400

    Add Contact, Update beacon and beacon current to have UUID with Major and Minor

--- tipless-cash-jar/db-tipless-cash-jar/dbo/Tables/beacon.sql
+++ tipless-cash-jar/db-tipless-cash-jar/dbo/Tables/beacon.sql
@@ -1,6 +1,8 @@
 ﻿CREATE TABLE [dbo].[beacon] (
     [beacon_key]      INT           IDENTITY (1, 1) NOT NULL,
-    [beacon_uid]      VARCHAR (100) NOT NULL,
+   	[beacon_uuid] [varchar](100) NOT NULL,
+	[major] [int] NOT NULL,
+	[minor] [int] NOT NULL,
     [beacon_type_uid] VARCHAR (30)  NOT NULL,
 	[beacon_issuer_key] INT NOT NULL,
     [create_dtm]      DATETIME      NOT NULL,
