dc09375b3d60ded944bb76b5f3c803051fcda14a
Shawn Wallace <shawn@the-wallaces.net>
2016-06-05 07:46:39 -0400

    Added skeleton of the recipient profile page. Also added MVC custom routing.

--- tipless-cash-jar/web/Controllers/HomeController.cs
+++ tipless-cash-jar/web/Controllers/HomeController.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using tiplessCashJar.web.Core;
 
-namespace web.Controllers
+namespace tiplessCashJar.web.Controllers
 {
-  public class HomeController : Controller
+  public class HomeController : AppControllerBase
   {
     public ActionResult Index()
     {
---
+++ tipless-cash-jar/web/Controllers/RecipientController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using tiplessCashJar.services;
+using tiplessCashJar.web.Core;
+
+namespace tiplessCashJar.web.Controllers
+{
+  public class RecipientController : AppControllerBase
+  {
+    //private IRecipientService RecipientService { get; set; }
+
+    //public RecipientController(IRecipientService recipientService) { RecipientService = recipientService; }
+
+    [HttpGet]
+    [Route("recipient/profile/{id:Guid}")]
+    public async Task<ActionResult> GetRecipientProfile(Guid id)
+    {
+      return View("Profile");
+    }
+
+  }
+}
