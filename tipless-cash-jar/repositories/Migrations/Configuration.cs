28517205558ea6323b928a2c3f44aff6ae2a9d58
jmflatt <jonathanm.flatt@gmail.com>
2016-06-04 20:42:54 -0700

    updated seeds to have authentic beacon info

--- tipless-cash-jar/repositories/Migrations/Configuration.cs
+++ tipless-cash-jar/repositories/Migrations/Configuration.cs
@@ -16,25 +16,25 @@ namespace tiplessCashJar.repositories.Migrations
 
         protected override void Seed(tiplessCashJar.repositories.TiplessCashJarContext context)
         {
-            //  This method will be called after migrating to the latest version.
+      //  This method will be called after migrating to the latest version.
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+      //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+      //  to avoid creating duplicate seed data. E.g.
+      //
+      //    context.People.AddOrUpdate(
+      //      p => p.FullName,
+      //      new Person { FullName = "Andrew Peters" },
+      //      new Person { FullName = "Brice Lambson" },
+      //      new Person { FullName = "Rowan Miller" }
+      //    );
+      //
+      context.Beacons.AddOrUpdate(
+         p => p.Name,
+         new BeaconEntity { Id = Guid.NewGuid(), Name = "badge 1", UUID = "DD915E3B-072C-4223-9A54-308390986793", Major = "57497", Minor = "25695", WhenCreated = DateTime.UtcNow },
+         new BeaconEntity { Id = Guid.NewGuid(), Name = "badge 2", UUID = "DD915E3B-072C-4223-9A54-308390986793", Major = "17356", Minor = "56347", WhenCreated = DateTime.UtcNow }
+       );
+    }
 
-          var old = context.Beacons.FirstOrDefault(m => m.Name == "abc123");
-          if (old != null)
-          {
-            context.Beacons.Remove(old);
-            context.SaveChanges();
-          }
-        }
+
     }
 }
